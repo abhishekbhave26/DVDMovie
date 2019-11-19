b5030fad892f3fc440bce54a50e8c868068fd35d
Abhishek Bhave <Abhishek.Bhave@sodexo.com>
2019-11-19 18:01:43 -0500

    UI Structure of Application

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,15 +6,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {ModelModule} from "./models/model.module";
 
+import { MovieTableComponent } from "./structure/movieTable.component";
+import { CategoryFilterComponent } from "./structure/categoryFilter.component";
+import { MovieDetailComponent } from "./structure/movieDetail.component";
+
+import { RoutingConfig } from "./app.routing";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieTableComponent,
+    CategoryFilterComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    ModelModule
+    ModelModule,
+    RoutingConfig
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
