3bb388daf4effd140ae35960f8d19df8620875fe
Abhishek Bhave <Abhishek.Bhave@sodexo.com>
2019-11-20 13:48:18 -0500

    Movie List, Pagination, Category Filtering component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,26 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {ModelModule} from "./models/model.module";
-
-import { MovieTableComponent } from "./structure/movieTable.component";
-import { CategoryFilterComponent } from "./structure/categoryFilter.component";
-import { MovieDetailComponent } from "./structure/movieDetail.component";
+import { StoreModule } from "./store/store.module";
+import { MovieSelectionComponent } from "./store/movieSelection.component";
 
 import { RoutingConfig } from "./app.routing";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieTableComponent,
-    CategoryFilterComponent,
-    MovieDetailComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ModelModule,
-    RoutingConfig
+    RoutingConfig,
+    StoreModule
     
   ],
   providers: [],
