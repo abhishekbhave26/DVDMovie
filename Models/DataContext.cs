b0ffeee3cd3dfde7c961a5062bd8546966bab4a2
Abhishek Bhave <Abhishek.Bhave@sodexo.com>
2019-11-25 16:21:43 -0500

    Order implemented

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -7,7 +7,9 @@ public DataContext(DbContextOptions<DataContext> opts)
     public DbSet<Movie> Movies {get; set;}
     public DbSet<Studio> Studios { get; set; }
     public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Order> Orders { get; set; }
     
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Movie>().HasMany<Rating>(m => m.Ratings)
